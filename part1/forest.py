401b3f859667bdcaffb1d0bd4793bf33b61c1cc9
codeandkey <jtst@iastate.edu>
2022-12-09 12:12:32 -0600

    forest: break long lines

--- part1/forest.py
+++ part1/forest.py
@@ -5,7 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 class RandomForest(Classifier):
     def __init__(self):
         super().__init__()
-        self.clf = RandomForestClassifier(n_estimators=args.trees, criterion=args.criterion)
+        self.clf = RandomForestClassifier(n_estimators=args.trees,
+                                          criterion=args.criterion)
 
     def train(self, samples, labels):
         self.clf.fit(samples, labels)
